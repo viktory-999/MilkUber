6f249093b2549df72e72369b8e8d36f61b97d788
Виктория Ширяева <viktoriasiraeva@macbook-air-viktoria.local>
2018-12-18 14:38:02 +0300

    Initial Commit

---
+++ MilkUber/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MilkUber
+//
+//  Created by Виктория Ширяева on 18.12.18.
+//  Copyright © 2018 Виктория Ширяева. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
