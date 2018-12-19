90db7620823bbd1630f5eab50e1d9a8bdba4892a
Виктория Ширяева <viktoriasiraeva@macbook-air-viktoria.local>
2018-12-19 16:58:13 +0300

    view did disappear

--- MilkUber/ViewController.swift
+++ MilkUber/ViewController.swift
@@ -18,7 +18,9 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         <#code#>
     }
-    
+    override func viewDidDisappear(_ animated: Bool) {
+        //
+    }
     
 }
 
