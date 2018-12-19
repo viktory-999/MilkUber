9b0fbd0134520296d1ccd0ba4f52cf8c3df3893f
Виктория Ширяева <viktoriasiraeva@macbook-air-viktoria.local>
2018-12-19 16:32:27 +0300

    add will appear

--- MilkUber/ViewController.swift
+++ MilkUber/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    override func viewWillAppear(_ animated: Bool) {
+        <#code#>
+    }
+    
+    
 }
 
